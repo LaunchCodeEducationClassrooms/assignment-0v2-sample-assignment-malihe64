86027124bb4c65f7f51d7b3fabf1c5834e7630f9
malihe64 <60053900+malihe64@users.noreply.github.com>
2021-02-14 00:22:40 -0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
+function ShowMessage(firstName, lastName) {
+    alert("Hello " + firstName + " " + lastName);
+}
 
+ShowMessage("Steve", "Jobs", "Mr."); // display Hello Steve Jobs
+ShowMessage("Bill"); // display Hello Bill undefined
+ShowMessage(); // display Hello undefined undefined
