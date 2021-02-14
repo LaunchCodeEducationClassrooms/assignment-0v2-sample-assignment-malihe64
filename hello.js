224a65f2058821fd70150796bf571437e2f42447
malihe64 <60053900+malihe64@users.noreply.github.com>
2021-02-14 01:46:09 -0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,5 +2,6 @@ function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
 	return "Hello";
-	return "Hello";
+	
 }
+module.exports = hello;
